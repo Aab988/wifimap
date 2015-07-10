81f3bffbed0f350703c7a8931a8295a9a3eb90b4
romanvlcek <roman.vlcek94@email.cz>
2015-07-10 17:13:42 +0200

    Google maps search
    Added google maps search function

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,7 @@
 /* global google */
 
     var map;
+var markers = [];
     
     function initialize() {
         var mapOptions = {center:{lat:49.5,lng:15.78},zoom:10, draggableCursor: 'default'};
@@ -29,6 +30,51 @@
                 infoWindow.open(map);
             });
         })
+
+   
+
+        var input = (document.getElementById('pac-input'));
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var searchBox = new google.maps.places.SearchBox((input));
+
+        google.maps.event.addListener(searchBox, 'places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            /*for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+            }
+
+            // For each place, get the icon, place name, and location.
+            markers = [];*/
+            var bounds = new google.maps.LatLngBounds();
+            for (var i = 0, place; place = places[i]; i++) {
+                var image = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: image,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+
+                markers.push(marker);
+
+                bounds.extend(place.geometry.location);
+            }
+
+            map.fitBounds(bounds);
+        });
     }
 
 
