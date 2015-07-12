289cb10547a9f76ff363a1a4525c5a7f57284bf4
romanvlcek <roman.vlcek94@email.cz>
2015-07-12 12:22:51 +0200

    New database structure

--- database_structure.sql
+++ database_structure.sql
@@ -15,10 +15,39 @@ CREATE DATABASE IF NOT EXISTS `wifimap` /*!40100 DEFAULT CHARACTER SET utf8 COLL
 USE `wifimap`;
 
 
+-- Exportování struktury pro tabulka wifimap.download_queue
+CREATE TABLE IF NOT EXISTS `download_queue` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_source` int(10) unsigned DEFAULT NULL,
+  `lat_start` double NOT NULL,
+  `lat_end` double NOT NULL,
+  `lon_start` double NOT NULL,
+  `lon_end` double NOT NULL,
+  `downloaded` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `calculated_nets_count` int(10) unsigned DEFAULT NULL,
+  `downloaded_nets_count` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_source_lat_start_lat_end_lon_start_lon_end` (`id_source`,`lat_start`,`lat_end`,`lon_start`,`lon_end`),
+  CONSTRAINT `FK_download_queue_source` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- Export dat nebyl vybrán.
+
+
+-- Exportování struktury pro tabulka wifimap.source
+CREATE TABLE IF NOT EXISTS `source` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Export dat nebyl vybrán.
+
+
 -- Exportování struktury pro tabulka wifimap.wifi
 CREATE TABLE IF NOT EXISTS `wifi` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_zdroj` int(11) unsigned NOT NULL DEFAULT '1',
+  `id_source` int(11) unsigned NOT NULL DEFAULT '1',
   `mac` char(17) NOT NULL,
   `ssid` varchar(255) NOT NULL,
   `sec` tinyint(4) NOT NULL,
@@ -39,22 +68,12 @@ CREATE TABLE IF NOT EXISTS `wifi` (
   `bcninterval` varchar(50) DEFAULT NULL,
   `qos` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_zdroj_mac_ssid_latitude_longtitude` (`id_zdroj`,`mac`,`ssid`,`latitude`,`longtitude`),
-  KEY `FK_wifi_zdroj` (`id_zdroj`),
+  UNIQUE KEY `id_zdroj_mac_ssid_latitude_longtitude` (`id_source`,`mac`,`ssid`,`latitude`,`longtitude`),
+  KEY `FK_wifi_zdroj` (`id_source`),
   KEY `latitude` (`latitude`),
   KEY `longtitude` (`longtitude`),
-  CONSTRAINT `FK_wifi_zdroj` FOREIGN KEY (`id_zdroj`) REFERENCES `zdroj` (`id`)
+  CONSTRAINT `FK_wifi_zdroj` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Export dat nebyl vybrán.
-
-
--- Exportování struktury pro tabulka wifimap.zdroj
-CREATE TABLE IF NOT EXISTS `zdroj` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nazev` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Export dat nebyl vybrán.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
