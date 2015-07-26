da474b9a72871ec2062da6fd1df8e69752acb309
romanvlcek <roman.vlcek94@email.cz>
2015-07-26 21:48:22 +0200

    code refactor

--- www/js/main.js
+++ www/js/main.js
@@ -8,6 +8,8 @@ var INIT_ZOOM = 8; // priblizeni mapy bez zjistene geolokace
 var INIT_CENTER = {lat:49.8,lng:15.5}; // vystredeni mapy bez zjistene geolokace
 var GEOLOCATION_ZOOM = 12; // zoom pri zjistene geolokaci
 
+var PROCESS_CLICK_URL = location.protocol + "//" + location.host + location.pathname + "wifi/processClick";
+var IMAGE_URL = location.protocol + "//" + location.host + location.pathname + "wifi/image";
 
 var markers = [];
 
@@ -18,9 +20,14 @@ var urlVars = getUrlVars();
 console.log(urlVars);
 
 var ssid = (urlVars["ssid"]) ? urlVars["ssid"] : "";
-console.log(ssid);
+init();
 
-    function initialize() {
+    function init() {
+        $("#form-ssid").val(ssid);
+
+
+    }
+    function initializeMap() {
 
         var mapOptions = {center:INIT_CENTER,zoom:INIT_ZOOM, draggableCursor: 'default'};
 	    map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
@@ -38,14 +45,11 @@ console.log(ssid);
 
         // odchytnuti kliknuti do mapy -> zobrazeni info okna
         google.maps.event.addListener(map, 'click', function(event) {
-            console.log(map.getZoom());
             if(map.getZoom() >= INFOWINDOW_MIN_ZOOM) {
-                var url = location.protocol + "//" + location.host + location.pathname + "wifi/processClick";
 
                 var bounds = map.getBounds();
-
                 $.ajax({
-                    url: url, data: {
+                    url: PROCESS_CLICK_URL, data: {
                         click_lat: event.latLng.lat(),
                         click_lon: event.latLng.lng(),
                         map_lat1: bounds.getSouthWest().lat(),
@@ -62,10 +66,9 @@ console.log(ssid);
                     infoWindow.open(map);
                 });
             }
+        });
 
-        })
-
-   
+        // google maps search
 
         var input = (document.getElementById('pac-input'));
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -112,9 +115,7 @@ console.log(ssid);
     }
 
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-    
-    
+    google.maps.event.addDomListener(window, 'load', initializeMap);
 
 function CoordMapType(tileSize) {
     this.tileSize = tileSize;
@@ -122,75 +123,51 @@ function CoordMapType(tileSize) {
 
 CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
 
-
     var bod = MERCATOR.getTileBounds({x: coord.x, y: coord.y, z: zoom});
     var img = ownerDocument.createElement('img');
-    var url = location.protocol + "//" + location.host + location.pathname + "wifi/image";
-    img.src = url+'?lat1='+bod.sw.lat+'&lat2='+bod.ne.lat+'&lon1='+bod.sw.lng + '&lon2='+bod.ne.lng + '&zoom='+zoom;
+    var params = {
+        lat1: bod.sw.lat,
+        lat2: bod.ne.lat,
+        lon1: bod.sw.lng,
+        lon2: bod.ne.lng,
+        zoom:zoom
+    };
     if(ssid != "") {
-        img.src += "&ssid="+ssid;
+        params.ssid = ssid;
     }
+
+    img.src = IMAGE_URL +"?" + $.param(params);
     img.alt = "wifimap";
     return img;
-  };
-  
-MERCATOR={
+};
 
-  fromLatLngToPoint:function(latLng){
-     var siny =  Math.min(Math.max(Math.sin(latLng.lat* (Math.PI / 180)),-.9999),.9999);
-     return {
-      x: 128 + latLng.lng * (256/360),
-      y: 128 + 0.5 * Math.log((1 + siny) / (1 - siny)) * -(256 / (2 * Math.PI))
-     };
-  },
-
-  fromPointToLatLng: function(point){
-     return {
-      lat: (2 * Math.atan(Math.exp((point.y - 128) / -(256 / (2 * Math.PI)))) - Math.PI / 2)/ (Math.PI / 180),
-      lng:  (point.x - 128) / (256 / 360)
-     };
-  },
-  getTileAtLatLng:function(latLng,zoom){
-    var t=Math.pow(2,zoom),
-        s=256/t,
-        p=this.fromLatLngToPoint(latLng);
-        return {x:Math.floor(p.x/s),y:Math.floor(p.y/s),z:zoom};
-  },
-
-  getTileBounds:function(tile){
-    tile=this.normalizeTile(tile);
-    var t=Math.pow(2,tile.z),
-        s=256/t,
-        sw={x:tile.x*s,
-            y:(tile.y*s)+s},
-        ne={x:tile.x*s+s,
-            y:(tile.y*s)};
-        return{sw:this.fromPointToLatLng(sw),
-               ne:this.fromPointToLatLng(ne)
-              }
-  },
-  normalizeTile:function(tile){
-    var t=Math.pow(2,tile.z);
-    tile.x=((tile.x%t)+t)%t;
-    tile.y=((tile.y%t)+t)%t;
-    return tile;
-  }
-
-}
-
-function form() {
-    console.log("submit");
-    ssid = $("#form-ssid").val();
-    window.location.hash = "ssid=" + ssid;
+function redrawOverlay() {
     map.overlayMapTypes.removeAt(0);
     map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
+}
+
+function searchFormSubmit() {
+    ssid = $("#form-ssid").val();
+    window.location.hash = $.param({mode: "SEARCH", ssid: ssid});
+    redrawOverlay();
     return false;
 }
 
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[#&]+([^=&]+)=([^&#]*)/gi, function(m,key,value) {
+    window.location.href.replace(/[#&]+([^=&]+)=([^&#]*)/gi, function(m,key,value) {
         vars[key] = value;
     });
     return vars;
+}
+
+/**
+ * zruseni vsech nastaveni - modu a jeho parametru
+ */
+function resetAllFilters() {
+    ssid = "";
+    window.location.hash = "";
+
+
+    redrawOverlay();
 }
