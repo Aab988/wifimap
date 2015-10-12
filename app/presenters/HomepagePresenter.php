a70191149cff953e1d134ac8f7a76927c3fdf7f6
romanvlcek <roman.vlcek94@email.cz>
2015-10-12 21:16:54 +0200

    right panel styles

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -43,8 +43,11 @@ class HomepagePresenter extends BasePresenter
 	protected function createComponentSearchForm() {
 		$form = new UI\Form;
 
+		$form->getElementPrototype()->class = "form-horizontal";
 		$ssidmacTxt = $form->addText('ssidmac', 'SSID/MAC:');
+		$ssidmacTxt->getControlPrototype()->class='form-control';;
 		//$ssidmac->getControlPrototype()->id='ssidmac';
+		$ssidmaclbl = $ssidmacTxt->getLabel()->addAttributes(array('class'=>'control-label col-sm-2'));
 
 		$channelsQ = $this->wifiManager->getAllChannels();
 		$channels = array();
@@ -52,14 +55,15 @@ class HomepagePresenter extends BasePresenter
 			$channels[$ch->channel] = $ch->channel;
 		}
 
-
 		$channelSelect = $form->addSelect('channel','Kanál:', $channels)
-			->setPrompt('Všechny kanály');
+			->setPrompt('Všechny kanály')
+			->getControlPrototype()->addAttributes(array('class'=>'form-control'));
 
 		$securitySelect = $form->addSelect('security', 'Zabezpečení:', array('Otevřená', 'WEP', 'WPA1', 'WPA2', 'jiné'))
-			->setPrompt("Všechny typy");
+			->setPrompt("Všechny typy")
+			->getControlPrototype()->addAttributes(array('class'=>'form-control'));
 
-		$form->addSubmit('search', 'Vyhledat');
+		$form->addSubmit('search', 'Vyhledat')->getControlPrototype()->addAttributes(array('class'=>'form-control btn btn-info btn-sm'));;
 		//$form->getElementPrototype()->onsubmit = 'return searchFormSubmit()';
 		//$form->getElementPrototype()->id = 'searchForm';
 		return $form;
